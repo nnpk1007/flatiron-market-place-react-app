5fe87be79fc0af3f992420d1a02209a2ab303678
Ngoc Nguyen <nnpk0410@gmail.com>
2023-07-08 13:06:45 -0500

    useState to track title, price, description, image in AddItem.js

--- src/component/AddItem.js
+++ src/component/AddItem.js
@@ -1,7 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 
 function AddItem() {
-
+    const [title, setTitle] = useState("")
+    const [price, setPrice] = useState("")
+    const [description, setDescription] = useState("")
+    const [image, setImage] = useState("")
+  return (
+    <div>
+      <h1>Add Item For Sale</h1>
+    </div>
+  );
 }
 
-export default AddItem
+export default AddItem;
