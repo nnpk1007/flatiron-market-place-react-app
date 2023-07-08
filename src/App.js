4e0fe1cec3b3786692d6978739bfcbc184df2901
Ngoc Nguyen <nnpk0410@gmail.com>
2023-07-08 15:00:15 -0500

    add feature to show login logout in App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
 
 import "bootstrap/dist/css/bootstrap.min.css";
@@ -7,6 +7,13 @@ import Listing from "./component/Listing";
 import AddItem from "./component/AddItem";
 
 function App() {
+  // state to track user logged in
+  const [loggedIn, setLoggIn] = useState(false)
+
+  const handleLogout = () => {
+    setLoggIn(false)
+  }
+
   return (
     <Router>
       <nav className="navbar navbar-expand-lg navbar-light bg-light mt-2">
@@ -21,9 +28,18 @@ function App() {
           <li className="nav-item">
             <Link className="nav-link" to="/add-item">Add Items</Link>
           </li>
-          {/* <li className="nav-item">
-            <Link className="nav-link" to="/login">Login</Link>
-          </li> */}
+          {/* if loggedin is true, then show Logout, if not, show Login*/}
+          {
+            loggedIn ? (
+              <li className="nav-item">
+                <button className="nav-link btn btn-link" onClick={handleLogout}>Logout</button>
+              </li>
+            ) : (
+              <li className="nav-item">
+                <button className="nav-link" to="/login">Login</button>
+              </li>
+            )
+          }
         </ul>
       </nav>
       <div className="container mt-4">
