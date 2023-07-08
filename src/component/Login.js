27ef10134d84cb390e4f637cbbeded1b5a3ee845
Ngoc Nguyen <nnpk0410@gmail.com>
2023-07-08 15:21:42 -0500

    add Login Form

--- src/component/Login.js
+++ src/component/Login.js
@@ -1,7 +1,22 @@
 import React from "react";
 
 function Login() {
-
+  return (
+    <div>
+      <h1>Login</h1>
+      <form>
+        <div className="form-group">
+          <label>Username</label>
+          <input type="text" className="form-control" required />
+          <div className="form-group">
+            <label>Password</label>
+            <input type="text" className="form-control" required />
+          </div>
+        </div>
+        <button type="submit" className="btn btn-primary">Login</button>
+      </form>
+    </div>
+  );
 }
 
-export default Login
+export default Login;
