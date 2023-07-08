d70b510d3c4e0c38861bdcab11fde65859559d83
Ngoc Nguyen <nnpk0410@gmail.com>
2023-07-08 15:48:50 -0500

    add handleLogin function

--- src/App.js
+++ src/App.js
@@ -10,56 +10,94 @@ import Login from "./component/Login";
 
 function App() {
   // state to track user logged in
-  const [loggedIn, setLoggedIn] = useState(false)
+  const [loggedIn, setLoggedIn] = useState(false);
+  // state to track username and password
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  // state error if username or password is valid or invalid
+  const [error, setError] = useState("");
+
+  const handleLogin = () => {
+    fetch("http://localhost:3001/users")
+      .then((r) => r.json())
+      .then((data) => {
+        const user = data.find(
+          (user) => user.username === username && user.password === password
+        );
+
+        if (user) {
+          setLoggedIn(true)
+          error("")
+        }
+        else {
+          setLoggedIn(false)
+          error("Invalid username or password")
+        }
+      });
+  };
 
   const handleLogout = () => {
-    setLoggedIn(false)
-  }
+    setLoggedIn(false);
+  };
 
   return (
     <Router>
       <nav className="navbar navbar-expand-lg navbar-light bg-light mt-2">
-      <Link className="navbar-brand" style={{ fontSize: "30px", color: "blue" }} to="/">Used Items For Sale</Link>
+        <Link
+          className="navbar-brand"
+          style={{ fontSize: "30px", color: "blue" }}
+          to="/"
+        >
+          Used Items For Sale
+        </Link>
         <ul className="navbar-nav mr-auto">
           <li className="nav-item active">
-            <Link className="nav-link" to="/">Home</Link>
+            <Link className="nav-link" to="/">
+              Home
+            </Link>
           </li>
           <li className="nav-item">
-            <Link className="nav-link" to="/listing">Listing</Link>
+            <Link className="nav-link" to="/listing">
+              Listing
+            </Link>
           </li>
           <li className="nav-item">
-            <Link className="nav-link" to="/add-item">Add Items</Link>
+            <Link className="nav-link" to="/add-item">
+              Add Items
+            </Link>
           </li>
           {/* if loggedin is true, then show Logout, if not, show Login*/}
-          {
-            loggedIn ? (
-              <li className="nav-item">
-                <button className="nav-link btn btn-link" onClick={handleLogout}>Logout</button>
-              </li>
-            ) : (
-              <li className="nav-item">
-                <button className="nav-link" to="/login">Login</button>
-              </li>
-            )
-          }
+          {loggedIn ? (
+            <li className="nav-item">
+              <button className="nav-link btn btn-link" onClick={handleLogout}>
+                Logout
+              </button>
+            </li>
+          ) : (
+            <li className="nav-item">
+              <button className="nav-link" to="/login">
+                Login
+              </button>
+            </li>
+          )}
         </ul>
       </nav>
       <div className="container mt-4">
-          <Switch>
-            <Route exact path={["/", "/home", "/listing"]}>
-              <Listing />
-            </Route>
-            <Route path="/add-item">
-              <AddItem />
-            </Route>
-            <Route path="/login">
-              {/* if login is true, go to homepage, else go to login */}
-              {(loggedIn) ? <Redirect to="/" /> : (<Login />)}
-            </Route>
-          </Switch>
+        <Switch>
+          <Route exact path={["/", "/home", "/listing"]}>
+            <Listing />
+          </Route>
+          <Route path="/add-item">
+            <AddItem />
+          </Route>
+          <Route path="/login">
+            {/* if login is true, go to homepage, else go to login */}
+            {loggedIn ? <Redirect to="/" /> : <Login />}
+          </Route>
+        </Switch>
       </div>
     </Router>
-  )
+  );
 }
 
-export default App
+export default App;
